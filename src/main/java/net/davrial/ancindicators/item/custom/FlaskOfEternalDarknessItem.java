0490611e2dd5b75418a6645117226db17e618541
Davrial <thebluemario@yahoo.com>
2024-12-18 21:08:21 -0600

    added tooltip explanations to the flask and vessel

--- src/main/java/net/davrial/ancindicators/item/custom/FlaskOfEternalDarknessItem.java
+++ src/main/java/net/davrial/ancindicators/item/custom/FlaskOfEternalDarknessItem.java
@@ -2,10 +2,15 @@
 package net.davrial.ancindicators.item.custom;
 
 import net.davrial.ancindicators.procedures.FoEDLightDarkeningProcedure;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class FlaskOfEternalDarknessItem extends Item {
     public FlaskOfEternalDarknessItem() {
@@ -31,5 +36,12 @@ public class FlaskOfEternalDarknessItem extends Item {
         super.useOn(context);
         FoEDLightDarkeningProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getItemInHand());
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+        super.appendHoverText(itemstack, level, list, flag);
+        list.add(Component.literal("§4Uncraftable, Creative Only"));
+        list.add(Component.literal("§4A for-fun item that spawns dark blocks when used on light sources"));
     }
 }
--- src/main/java/net/davrial/ancindicators/item/custom/VesselOfInfiniteLightItem.java
+++ src/main/java/net/davrial/ancindicators/item/custom/VesselOfInfiniteLightItem.java
@@ -2,10 +2,15 @@
 package net.davrial.ancindicators.item.custom;
 
 import net.davrial.ancindicators.procedures.VoILLightingProcedure;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class VesselOfInfiniteLightItem extends Item {
     public VesselOfInfiniteLightItem() {
@@ -31,5 +36,14 @@ public class VesselOfInfiniteLightItem extends Item {
         super.useOn(context);
         VoILLightingProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getItemInHand());
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+        super.appendHoverText(itemstack, level, list, flag);
+        list.add(Component.literal("§4Uncraftable, Creative Only"));
+        list.add(Component.literal("§4A for-fun item that spawns light blocks"));
+        list.add(Component.literal("§7Unfortunately does not currently work. Dark does, Light doesnt"));
+        list.add(Component.literal("§7No idea why as I changed nothing since last testing both"));
     }
 }
